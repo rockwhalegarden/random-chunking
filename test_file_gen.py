1004811cec4355aa4aaa974dece976e6220b8fd1
unknown <dolphin2410@outlook.kr>
2025-02-17 21:15:01 +0900

    update test_file_gen

--- test_file_gen.py
+++ test_file_gen.py
@@ -1,6 +1,5 @@
 import os
 
-def test_file_gen():
-    with open("gigabyte.bin", 'wb') as f:
-        for _ in range(1024):
-            f.write(os.urandom(1024 * 1024))
+with open("gigabyte.bin", 'wb') as f:
+    for _ in range(1024):
+        f.write(os.urandom(1024 * 1024))
